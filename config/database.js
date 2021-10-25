7c09bfd79059f27aa96052109b0536df524ba1e0
Monica Contreras <78456343+Mcontre11@users.noreply.github.com>
2021-10-24 21:10:27 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://Mcontreras11:dTxbnYBe2XZjRcGJ@cluster0.2exur.mongodb.net/cluster0?retryWrites=true&w=majority', 
+    'url' : '', 
     'dbName': 'rouletteApp'
 };
