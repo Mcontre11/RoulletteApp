80087af8887d2a6bffba080f385ae8739bb6e9f0
Resilient Coders <resilientcoders@admins-MacBook-Pro.local>
2021-10-27 17:25:30 -0400

    DBcommit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+
+   
+module.exports = {
+
+  'url' : 'mongodb+srv://Mcontreras11:dTxbnYBe2XZjRcGJ@cluster0.2exur.mongodb.net/cluster0?retryWrites=true&w=majority', 
+  'dbName': 'cluster 0', 
+
+};
